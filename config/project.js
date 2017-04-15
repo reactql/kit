fddf1259230c19fb11e2280cfc6a2f660fb372c8
Lee Benson <lee@leebenson.com>
2017-04-15 15:30:23 +0100

    initial commit

---
+++ config/project.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+
+export const APOLLO = {
+  uri: 'https://api.graph.cool/simple/v1/cinomw2r1018601o42x5z69uc',
+};
+
+export const BUNDLE_ANALYZER = {
+  openAnalyzer: false,
+};
